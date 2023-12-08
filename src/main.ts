a9ab71632101a35d53a91c4ecdd17b0e6dc3d2b4
momorsi55573 <101499489+momorsi55573@users.noreply.github.com>
2023-12-08 15:17:39 +0200

    1

--- src/main.ts
+++ src/main.ts
@@ -16,6 +16,6 @@ async function bootstrap() {
   app.useStaticAssets(join(__dirname, '..', 'src', 'public'));
   app.setBaseViewsDir(join(__dirname, '..', 'src', 'views'));
   app.setViewEngine('ejs');
-  await app.listen(3000 || process.env.PORT);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
