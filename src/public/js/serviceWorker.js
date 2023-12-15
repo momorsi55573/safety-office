8efa19800dc8620f9ec341d574a414bb28006936
momorsi55573 <101499489+momorsi55573@users.noreply.github.com>
2023-12-15 15:55:45 +0200

    1

--- src/public/js/serviceWorker.js
+++ src/public/js/serviceWorker.js
@@ -1,2 +1,28 @@
 /* eslint-disable prettier/prettier */
-console.log('Service worker loded:');
+const urlBase64ToUint8Array = (base64String) => {
+    const padding = '='.repeat((4 - base64String.length % 4) % 4);
+    // eslint-disable-next-line no-useless-escape
+    const base64 = (base64String + padding).replace(/\-/g, '+').replace(/_/g, '/');
+    const rawData = atob(base64);
+    const outPutArray = new Uint8Array(rawData.length);
+    for (let i = 0; i < rawData.length; i++) {
+        outPutArray[i] = rawData.charCodeAt(i);
+        
+    }
+    return outPutArray;
+    };
+self.addEventListener('activate', async (e) => {
+  const subscription = await self.registration.pushManager.subscribe({
+    userVisibleOnly: true,
+    applicationServerKey: urlBase64ToUint8Array(
+        'BEiMjwCnIJ1Ns3Voriu2cpL1x4hr4mioj3Pb-pehWOxJmfj8bOrEccOp0zdWsepAqrU2y77Y_KgF4d6IwPTmSvs'
+    )
+  });
+  console.log(subscription)
+});
+
+//Public Key:
+//BEiMjwCnIJ1Ns3Voriu2cpL1x4hr4mioj3Pb-pehWOxJmfj8bOrEccOp0zdWsepAqrU2y77Y_KgF4d6IwPTmSvs
+
+//Private Key:
+//iRPrLMnuZFWOgRE_vZ8Rhane-GY_F05X8gpm_Ru22mY
