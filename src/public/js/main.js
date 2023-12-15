842d5cbb660b219020b7b30875b1f28596711661
momorsi55573 <101499489+momorsi55573@users.noreply.github.com>
2023-12-15 12:12:54 +0200

    1

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -1,35 +1,14 @@
 /* eslint-disable prettier/prettier */
-
-
-async function registerServiceWorker() {
-    await navigator.serviceWorker.register('../public/js/serviceWorker.js')
-    updateUI();
-  }
-  
-  function getRegistration() {
-    return navigator.serviceWorker.getRegistration();
-  }
-  
-  // Create and send a test notification to the service worker.
-async function sendNotification() {
-    // Use a random number as part of the notification data
-    // (so you can tell the notifications apart during testing!)
-    let randy = Math.floor(Math.random() * 100);
-    let notification = {
-      title: 'Test ' + randy,
-      options: { body: 'Test body ' + randy }
-    };
-    // Get a reference to the service worker registration.
-    let registration = await getRegistration();
-    // Check that the service worker registration exists.
-    if (registration) {
-      // Check that a service worker controller exists before
-      // trying to access the postMessage method.
-      if (navigator.serviceWorker.controller) {
-        navigator.serviceWorker.controller.postMessage(notification);
-      } else {
-        console.log('No service worker controller found. Try a soft reload.');
-      }
-    }
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+      navigator.serviceWorker
+        .register('../public/js/serviceWorker.js')
+        .then((registration) => {
+          console.log('Service worker registered:', registration);
+        })
+        .catch((error) => {
+          console.log('Service worker registration failed:', error);
+        });
+    });
   }
   
--- src/public/js/serviceWorker.js
+++ src/public/js/serviceWorker.js
@@ -1,13 +1,23 @@
 /* eslint-disable prettier/prettier */
-// Show notification when received
-console.log('Service worker loaded.');
-self.addEventListener('message', (event) => {
-    let notification = event.data;
-    self.registration.showNotification(
-      notification.title,
-      notification.options
-    ).catch((error) => {
-      console.log(error);
-    });
-  });
-  
+console.log('hello:', registration);
+self.addEventListener('push', function (event) {
+  const options = {
+    body: event.data.text(),
+    icon: '/path/to/icon.png',
+    badge: '/path/to/badge.png',
+    vibrate: [200, 100, 200],
+    data: {
+      url: event.data.url,
+    },
+  };
+
+  event.waitUntil(
+    self.registration.showNotification('Notification Title', options),
+  );
+});
+
+self.addEventListener('notificationclick', function (event) {
+  event.notification.close();
+
+  event.waitUntil(clients.openWindow(event.notification.data.url));
+});
