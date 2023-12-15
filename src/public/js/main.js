bb5e9885a045e2e320ef428d43420beda2f68416
momorsi55573 <101499489+momorsi55573@users.noreply.github.com>
2023-12-15 13:08:53 +0200

    1

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -1,15 +1,30 @@
 /* eslint-disable prettier/prettier */
 // Check if the browser supports service workers
-if ('serviceWorker' in navigator) {
-  // Register the service worker
-  navigator.serviceWorker
-    .register('../public/js/serviceWorker.js')
-    .then((registration) => {
-      console.log('Service worker registered:', registration);
-    })
-    .catch((error) => {
-      console.error('Service worker registration failed:', error);
-    });
-} else {
-  console.log('Service workers are not supported.');
+const CheckPermission = () => {
+    if (!('serviceWorker' in navigator)) {
+        throw new Error('No Service Worker support!');
+    }
+    if (!('notifications' in window)) {
+        throw new Error('No api support!');
+    }
 }
+
+const registerServiceWorker = async () => {
+    const swRegistration = await navigator.serviceWorker.register('../public/js/serviceWorker.js');
+    return swRegistration;
+}
+
+const requestNotificationPermission = async () => {
+    const permission = await window.Notification.requestPermission();
+    // value of permission can be 'granted', 'default', 'denied'
+    // granted: user has accepted the request
+    // default: user has dismissed the notification permission popup by clicking on x
+    // denied: user has denied the request.
+    if (permission !== 'granted') {
+        throw new Error('Permission not granted for Notification');
+    }else{ new Notification('Hi there!');}
+}
+
+CheckPermission();
+registerServiceWorker();
+requestNotificationPermission();
